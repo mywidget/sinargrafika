2bd63acdef5f1cea0f2dc19df777a505928b5fcb
Munajat Ibnu <78370273+mywidget@users.noreply.github.com>
2026-09-10 23:46:00 +0700

    Update PrintPro template ke v1.1.14

--- sw.js
+++ sw.js
@@ -1,5 +1,9 @@
 
-const CACHE_NAME = 'printpro-cache-v1789042350565';
+<<<<<<< Updated upstream
+const CACHE_NAME = 'printpro-cache-v1789056890516';
+=======
+const CACHE_NAME = 'printpro-cache-v1788973855609';
+>>>>>>> Stashed changes
 const ASSETS_TO_CACHE = [
   '/',
   '/index.html'
